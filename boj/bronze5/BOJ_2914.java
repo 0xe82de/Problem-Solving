25639e05e5f292f3cf72a26b0ca9641f48a7ee4c
김경석 <0xe82de@gmail.com>
2021-08-01 01:57:17 +0900

    BOJ_2914

---
+++ boj/bronze5/BOJ_2914.java
@@ -0,0 +1,19 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class BOJ_2914 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int I = sc.nextInt();
+		sc.close();
+		
+		System.out.println(A * (I - 1) + 1);
+		
+	}
+
+}
