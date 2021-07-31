bf42d57d37a5d98d0781344611df838f78befbaa
김경석 <0xe82de@gmail.com>
2021-07-31 23:53:02 +0900

    BOJ_2845

---
+++ boj/bronze5/BOJ_2845.java
@@ -0,0 +1,28 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class BOJ_2845 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		final int N = 5;
+		int[] peaples = new int[N];
+		
+		int L = sc.nextInt();
+		int P = sc.nextInt();
+		
+		for (int i = 0; i < N; ++i) {
+			peaples[i] = sc.nextInt();
+		}
+		sc.close();
+		
+		for (int i = 0; i < N; ++i) {
+			System.out.print(peaples[i] - (L * P) + " ");
+		}
+		
+	}
+
+}
