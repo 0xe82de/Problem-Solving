65ad97397981284706100c994b0722f4ccf96bf8
김경석 <0xe82de@gmail.com>
2021-08-01 21:45:37 +0900

    Test.java

--- boj/solving/Test.java
+++ boj/solving/Test.java
@@ -1,8 +1,14 @@
 package solving;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		
 		
