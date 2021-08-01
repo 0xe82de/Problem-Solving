1a8e2c6439c12576d47f6daebf2a52194413125d
김경석 <0xe82de@gmail.com>
2021-08-02 02:45:16 +0900

    Test

--- boj/solving/Test.java
+++ boj/solving/Test.java
@@ -1,15 +1,16 @@
 package solving;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
 public class Test {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		
+		String s = "0123456789";
+		
+		int i = 5;
+		
+		s = s.substring(0, i) + 'D' + s.substring(i + 1);
+		
+		System.out.println(s);
 		
 		
 	}
