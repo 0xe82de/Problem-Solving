ea8c8adcf7f2e759e879183d2cf08d049b60c9ae
김경석 <0xe82de@gmail.com>
2021-08-02 17:52:13 +0900

    BOJ_11729 start

--- boj/solving/BOJ_11729.java
+++ boj/solving/BOJ_11729.java
@@ -1,9 +1,19 @@
 package solving;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BOJ_11729 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = br.read() - '0';
 		
 		
 	}
