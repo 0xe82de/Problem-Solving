702fb69e1e0bf8dc5966e2000ec01b6f433ec76f
김경석 <0xe82de@gmail.com>
2021-08-02 17:29:43 +0900

    Test.java

--- boj/solving/Test.java
+++ boj/solving/Test.java
@@ -4,13 +4,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		String s = "0123456789";
-		
-		int i = 5;
-		
-		s = s.substring(0, i) + 'D' + s.substring(i + 1);
-		
-		System.out.println(s);
 		
 		
 	}
