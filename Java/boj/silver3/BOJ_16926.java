0c961703b69bf61b9ed54bb4cbbb687ce0051e4d
김경석 <0xe82de@gmail.com>
2021-08-11 17:13:29 +0900

    BOJ_16926 complete

--- Java/boj/silver3/BOJ_16926.java
+++ Java/boj/silver3/BOJ_16926.java
@@ -17,25 +17,28 @@ public class BOJ_16926 {
 			
 			// 맨위
 			// 왼쪽으로 이동
-			for (int i = src; i < baseCol - src - 1; ++i) {
+			int dstCol = baseCol - src - 1;
+			int dstRow = baseRow - src - 1;
+			
+			for (int i = src; i < dstCol; ++i) {
 				arr[src][i] = arr[src][i + 1];
 			}
 			
 			// 맨오른쪽
 			// 위쪽으로 이동
-			for (int i = src; i < baseRow - src - 1; ++i) {
-				arr[i][baseCol - src - 1] = arr[i + 1][baseCol - src - 1];
+			for (int i = src; i < dstRow; ++i) {
+				arr[i][dstCol] = arr[i + 1][dstCol];
 			}
 			
 			// 맨밑
 			// 오른쪽으로 이동
-			for (int i = baseCol - src - 1; i > src; --i) {
-				arr[baseRow - src - 1][i] = arr[baseRow - src - 1][i - 1];
+			for (int i = dstCol; i > src; --i) {
+				arr[dstRow][i] = arr[dstRow][i - 1];
 			}
 			
 			// 맨왼쪽
 			// 밑으로 이동
-			for (int i = baseRow - src - 1; i > src + 1; --i) {
+			for (int i = dstRow; i > src + 1; --i) {
 				arr[i][src] = arr[i - 1][src];
 			}
 			
