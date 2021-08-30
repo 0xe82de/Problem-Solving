7fd61120c71fac99497e9f8de8ce05910d9e01ed
0xe82de <0xe82de@gmail.com>
2021-08-31 00:11:04 +0900

    BOJ_3052_1 complete

---
+++ Java/boj/bronze/BOJ_3052_1.java
@@ -0,0 +1,30 @@
+package boj.bronze;
+
+import java.util.Scanner;
+
+public class BOJ_3052_1 {
+
+	public static void main(String[] args) {
+		
+		// io
+		Scanner sc = new Scanner(System.in);
+		
+		final int N = 42;
+		int[] arr = new int[N];
+		
+		for (int i = 0; i < 10; ++i) {
+			arr[sc.nextInt() % 42] = 1;
+		}
+		
+		int sum = 0;
+		for (int i = 0; i < N; ++i) {
+			sum += arr[i];
+		}
+		
+		System.out.print(sum);
+		
+		// io close
+		sc.close();
+	}
+
+}
