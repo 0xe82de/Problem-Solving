27082666b1834cab32e1befe970b6e43272ee749
0xe82de <0xe82de@gmail.com>
2021-08-30 19:49:38 +0900

    BOJ_2741_1 complete

--- Java/solving/BOJ_2741_1.java
+++ Java/solving/BOJ_2741_1.java
@@ -1,10 +1,22 @@
 package solving;
 
+import java.util.Scanner;
+
 public class BOJ_2741_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// io
+		Scanner sc = new Scanner(System.in);
+		
+		final int N = sc.nextInt();
+		
+		for (int i = 1; i <= N; ++i) {
+			System.out.println(i);
+		}
+		
+		// io close
+		sc.close();
 	}
 
 }
