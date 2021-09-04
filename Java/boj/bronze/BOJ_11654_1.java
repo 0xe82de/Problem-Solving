add4bce2f54c18010673357b12effc4f529164cb
0xe82de <0xe82de@gmail.com>
2021-09-04 21:02:46 +0900

    BOJ_11654_1 complete

---
+++ Java/boj/bronze/BOJ_11654_1.java
@@ -0,0 +1,20 @@
+package boj.bronze;
+
+import java.util.Scanner;
+
+public class BOJ_11654_1 {
+
+	public static void main(String[] args) {
+		
+		// io
+		Scanner sc = new Scanner(System.in);
+		
+		char ch = sc.next().charAt(0);
+		
+		System.out.printf("%d", (int)ch);
+		
+		// io close
+		sc.close();
+	}
+
+}
