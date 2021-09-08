74b637dee46cd4bf09e960ed40b1f7a7e23881e5
0xe82de <0xe82de@gmail.com>
2021-09-09 00:00:13 +0900

    complete

---
+++ Java/boj/bronze/BOJ_11718_1.java
@@ -0,0 +1,31 @@
+package boj.bronze;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_11718_1 {
+
+	public static void main(String[] args) throws IOException {
+		
+		// io
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringBuilder sb = new StringBuilder();
+		
+		String temp = new String();
+		while ((temp = br.readLine()) != null) {
+			sb.append(temp + "\n");
+		}
+		
+		// output
+		bw.write(sb.toString());
+
+		// io close
+		bw.close();
+		br.close();
+	}
+}
