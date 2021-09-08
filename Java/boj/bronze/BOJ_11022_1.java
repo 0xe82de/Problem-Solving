49966917f7683ac9cde620a72755f1335638a48c
0xe82de <0xe82de@gmail.com>
2021-09-08 09:36:09 +0900

    complete

---
+++ Java/boj/bronze/BOJ_11022_1.java
@@ -0,0 +1,39 @@
+package boj.bronze;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_11022_1 {
+
+	public static void main(String[] args) throws IOException {
+		
+		// io
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		// 테스트 케이스 개수
+		final int TC = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1; tc <= TC; ++tc) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int num1 = Integer.parseInt(st.nextToken());
+			int num2 = Integer.parseInt(st.nextToken());
+			
+			sb.append("Case #" + tc + ": " + num1 + " + " + num2 + " = " + (num1 + num2) + "\n");
+		}
+		
+		// output
+		bw.write(sb.toString());
+		
+		// io close
+		bw.close();
+		br.close();
+	}
+}
