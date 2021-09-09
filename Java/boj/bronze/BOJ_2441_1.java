845b7835ee094ec89f34baad93c7c9dfb4a76100
0xe82de <0xe82de@gmail.com>
2021-09-10 01:26:43 +0900

    complete

---
+++ Java/boj/bronze/BOJ_2441_1.java
@@ -0,0 +1,40 @@
+package boj.bronze;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_2441_1 {
+
+	public static void main(String[] args) throws IOException {
+		
+		// io
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringBuilder sb = new StringBuilder();
+		
+		final int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; ++i) {
+			
+			for (int j = 0; j < i; ++j) {
+				sb.append(" ");
+			}
+			
+			for (int j = N; j > i; --j) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		
+		// output
+		bw.write(sb.toString());
+
+		// io close
+		bw.close();
+		br.close();
+	}
+}
--- Java/solving/BOJ_2441_1.java
+++
@@ -1,8 +0,0 @@
-package solving;
-
-public class BOJ_2441_1 {
-	
-	public static void main(String[] args) {
-		
-	}
-}
