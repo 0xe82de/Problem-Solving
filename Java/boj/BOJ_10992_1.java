9104ffc47514be433044af47470e757ff2613afc
0xe82de@gmail.com <0xe82de@gmail.com>
2021-09-12 20:57:22 +0900

    complete
    문제를 잘못 이해하였음
    
    잘못 이해한 모양
    N => 5
        *
       * *
      * * *
     *     *
    *********
    
    문제에서 요구하는 모양
        *
       * *
      *   *
     *     *
    *********

---
+++ Java/boj/BOJ_10992_1.java
@@ -0,0 +1,43 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_10992_1 {
+
+	public static void main(String[] args) throws IOException {
+		
+		// io
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringBuilder sb = new StringBuilder();
+		
+		final int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; ++i) {
+			
+			for (int j = i; j < N - 1; ++j) sb.append(" ");
+			sb.append("*");
+			
+			if (i == N - 1) {
+				for (int j = 0, count = 2 * i - 1; j < count; ++j) sb.append("*");
+			} else if (i > 0) {
+				for (int j = 0, count = 2 * i - 1; j < count; ++j) sb.append(" ");
+			}
+			
+			if (i > 0) sb.append("*");
+			sb.append("\n");
+		}
+		
+		// output
+		bw.write(sb.toString());
+
+		// io close
+		bw.close();
+		br.close();
+	}
+}
