05b93cd8f3c6ffbc191fc52bed3296519526dc1d
0xe82de@gmail.com <0xe82de@gmail.com>
2021-09-17 14:57:38 +0900

    start

---
+++ Java/solving/SWEA_1767_1.java
@@ -0,0 +1,15 @@
+package solving;
+
+/*
+ * SWEA 1767
+ * SW Test 샘플문제
+ */
+
+public class SWEA_1767_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
