54ca5b24afceecf9694db8de62f700d5996a753d
0xe82de@gmail.com <0xe82de@gmail.com>
2021-09-18 02:07:03 +0900

    complete

---
+++ Java/boj/silver/BOJ_11726_1.java
@@ -0,0 +1,39 @@
+package boj.silver;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/*
+ * BOJ 11726 2xn 타일링
+ */
+
+public class BOJ_11726_1 {
+
+	public static void main(String[] args) throws IOException {
+		
+		// io
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		final int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N + 1];
+		
+		arr[0] = 1;
+		arr[1] = 1;
+		
+		for (int i = 2; i <= N; ++i) {
+			arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
+		}
+
+		// output
+		bw.write(String.valueOf(arr[N] % 10007));
+
+		// io close
+		bw.close();
+		br.close();
+	}
+}
