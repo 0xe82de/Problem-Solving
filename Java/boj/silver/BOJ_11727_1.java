813c99dcef81ee13784e49ffb94bbe44651546f8
0xe82de@gmail.com <0xe82de@gmail.com>
2021-09-18 02:23:35 +0900

    complete

--- Java/boj/silver/BOJ_11727_1.java
+++ Java/boj/silver/BOJ_11727_1.java
@@ -1,4 +1,10 @@
-package solving;
+package boj.silver;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 /*
  * BOJ 11727 2xn 타일링 2
@@ -6,9 +12,28 @@ package solving;
 
 public class BOJ_11727_1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+		// io
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		final int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N + 1];
+		
+		arr[0] = 1;
+		arr[1] = 1;
+		
+		for (int i = 2; i <= N; ++i) {
+			arr[i] = (arr[i - 1] + arr[i - 2] * 2) % 10007;
+		}
 
+		// output
+		bw.write(String.valueOf(arr[N]));
+
+		// io close
+		bw.close();
+		br.close();
 	}
-
 }
