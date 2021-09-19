b852f3156c0a6e620660d03150492354ce023d54
0xe82de@gmail.com <0xe82de@gmail.com>
2021-09-19 20:05:17 +0900

    start

---
+++ Java/solving/BOJ_9095_1.java
@@ -0,0 +1,15 @@
+package solving;
+
+/*
+ * BOJ 9095 1, 2, 3 더하기
+ * S3
+ */
+
+public class BOJ_9095_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
