d02c6b5f8dabbb9188a3fdfbb2d5619284ae85aa
0xe82de@gmail.com <0xe82de@gmail.com>
2021-09-21 19:13:53 +0900

    start

---
+++ Java/solving/BOJ_1699_1.java
@@ -0,0 +1,16 @@
+package solving;
+
+/*
+ * BOJ 1699 제곱수의 합
+ * S3
+ * DP
+ */
+
+public class BOJ_1699_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
