6f9f5e5db9ac7a966b10b5a8f666e2f8d36dd1d8
0xe82de@gmail.com <0xe82de@gmail.com>
2021-09-22 18:20:19 +0900

    start

---
+++ Java/solving/BOJ_1065_1.java
@@ -0,0 +1,17 @@
+package solving;
+
+/*
+ * BOJ 1065 한수
+ * S4
+ * 브루트포스
+ * 단계별로 풀어보기 : 함수
+ */
+
+public class BOJ_1065_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
