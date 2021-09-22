f31ef3d4cd416a074c04a0685ce28b877a89f791
0xe82de@gmail.com <0xe82de@gmail.com>
2021-09-22 20:53:09 +0900

    start

---
+++ Java/solving/BOJ_1712_1.java
@@ -0,0 +1,17 @@
+package solving;
+
+/*
+ * BOJ 1712 손익분기점
+ * B4
+ * 수학, 사칙연산
+ * 단계별로 풀어보기 : 기본 수학 1
+ */
+
+public class BOJ_1712_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
