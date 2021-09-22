843713a82fa75450e1b668eaa084425296ce4ef7
0xe82de@gmail.com <0xe82de@gmail.com>
2021-09-22 17:00:28 +0900

    start

---
+++ Java/solving/BOJ_1110_1.java
@@ -0,0 +1,16 @@
+package solving;
+
+/*
+ * BOJ 1110 더하기 사이클
+ * B1
+ * 구현
+ */
+
+public class BOJ_1110_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
