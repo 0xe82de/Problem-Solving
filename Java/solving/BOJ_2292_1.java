ba1bae0c3f78d296f27533f21ac5a50eb0dff7c5
0xe82de@gmail.com <0xe82de@gmail.com>
2021-09-23 10:56:01 +0900

    start

---
+++ Java/solving/BOJ_2292_1.java
@@ -0,0 +1,17 @@
+package solving;
+
+/*
+ * BOJ 2292 벌집
+ * B2
+ * 수학
+ * 단계별로 풀어보기 : 기본 수학 1
+ */
+
+public class BOJ_2292_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
