e6805ecd518292e7c16237cd8898ae31e702fe51
0xe82de@gmail.com <0xe82de@gmail.com>
2021-09-24 14:23:00 +0900

    start

---
+++ Java/solving/BOJ_17144_1.java
@@ -0,0 +1,16 @@
+package solving;
+
+/*
+ * BOJ 17144 미세먼지 안녕!
+ * G4
+ * 구현
+ */
+
+public class BOJ_17144_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
