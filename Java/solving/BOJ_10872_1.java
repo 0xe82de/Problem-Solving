590d62d7fc377622d38d11d835158737d4f17988
0xe82de@gmail.com <0xe82de@gmail.com>
2021-09-25 22:20:45 +0900

    start

---
+++ Java/solving/BOJ_10872_1.java
@@ -0,0 +1,16 @@
+package solving;
+
+/*
+ * BOJ 10872 팩토리얼
+ * B3
+ * 수학, 구현
+ */
+
+public class BOJ_10872_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
