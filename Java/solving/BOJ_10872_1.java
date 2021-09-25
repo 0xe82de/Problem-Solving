43c8831cd70ff548e3a44fae4687ad3f48f46c95
0xe82de@gmail.com <0xe82de@gmail.com>
2021-09-25 22:25:57 +0900

    success / BOJ 10872 팩토리얼

--- Java/solving/BOJ_10872_1.java
+++ Java/solving/BOJ_10872_1.java
@@ -1,5 +1,11 @@
 package solving;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 /*
  * BOJ 10872 팩토리얼
  * B3
@@ -8,9 +14,24 @@ package solving;
 
 public class BOJ_10872_1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+		// io
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		final int N = Integer.parseInt(br.readLine());
 
+		// output
+		bw.write(String.valueOf(fact(N)));
+
+		// io close
+		bw.close();
+		br.close();
 	}
-
+	
+	private static int fact(int n) {
+		if (n <= 1) return 1;
+		else return n * fact(n - 1);
+	}
 }
