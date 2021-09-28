4220da63b5d4a978e9fb5bedbb178ddeb5394820
0xe82de@gmail.com <0xe82de@gmail.com>
2021-09-28 15:45:43 +0900

    start

---
+++ Java/solving/BOJ_10250_1.java
@@ -0,0 +1,16 @@
+package solving;
+
+/*
+ * BOJ 10250 ACM 호텔
+ * B3
+ * 수학, 구현, 사칙연산
+ */
+
+public class BOJ_10250_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
