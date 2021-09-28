ec50238882b0ec11aed21d7b08a8cc733af458a4
0xe82de@gmail.com <0xe82de@gmail.com>
2021-09-28 18:47:59 +0900

    start

---
+++ Java/solving/SWEA_5607_1.java
@@ -0,0 +1,16 @@
+package solving;
+
+/*
+ * SWEA 5607 조합
+ * D3
+ * 수학
+ */
+
+public class SWEA_5607_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
