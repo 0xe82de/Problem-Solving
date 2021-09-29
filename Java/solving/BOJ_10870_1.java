97e4f7ce3c7caf5d5c695f7d9d2ee47deb4b461a
0xe82de@gmail.com <0xe82de@gmail.com>
2021-09-29 16:32:35 +0900

    Success / BOJ 10870 피보나치 수 5

--- Java/solving/BOJ_10870_1.java
+++ Java/solving/BOJ_10870_1.java
@@ -1,5 +1,7 @@
 package solving;
 
+import java.util.Scanner;
+
 /**
  * BOJ 10870 피보나치 수 5
  * B2
@@ -10,6 +12,20 @@ public class BOJ_10870_1 {
 
     public static void main(String[] args) {
 
+        // io
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        if (n == 0) System.out.println(0);
+        else System.out.println(fibo(n - 1));
+
+        // io close
+        sc.close();
+    }
+
+    private static int fibo(int n) {
+        if (n <= 1) return 1;
+        return fibo(n - 2) + fibo(n - 1);
     }
 
 }
