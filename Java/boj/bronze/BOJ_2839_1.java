b45306c62e9a7e6e235fe5c7e5b9ac12a2a84654
0xe82de@gmail.com <0xe82de@gmail.com>
2021-10-12 23:59:38 +0900

    Success / BOJ 2839 설탕 배달

---
+++ Java/boj/bronze/BOJ_2839_1.java
@@ -0,0 +1,41 @@
+package boj.bronze;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * BOJ 2839 설탕 배달
+ * B1
+ * 수학, DP, 그리디
+ */
+
+public class BOJ_2839_1 {
+
+    public static void main(String[] args) throws IOException {
+        // io
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // logic
+        final int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[5000 + 1];
+        dp[3] = dp[5] = 1;
+
+        for (int i = 6; i <= N; ++i) {
+            if (dp[i - 3] > 0) dp[i] = dp[i - 3] + 1;
+            if (dp[i - 5] > 0) {
+                if (dp[i - 3] != 0) dp[i] = Math.min(dp[i], dp[i - 5] + 1);
+                else dp[i] = dp[i - 5] + 1;
+            }
+        }
+        if (dp[N] == 0) dp[N] = -1;
+
+        // output
+        bw.write(String.valueOf(dp[N]));
+
+        // io close
+        bw.close();
+        br.close();
+    }
+
+}
