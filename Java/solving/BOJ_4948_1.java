14bbdeb5f6455ce33551c78a332a40a9e6065ef3
0xe82de@gmail.com <0xe82de@gmail.com>
2021-10-19 13:25:28 +0900

    init

---
+++ Java/solving/BOJ_4948_1.java
@@ -0,0 +1,33 @@
+package solving;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * BOJ 4948 베르트랑 공준
+ * S2
+ * 수학, 정수론, 소수 판정, 에라토스테네스의 체
+ */
+
+public class BOJ_4948_1 {
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+        // io
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        // logic
+
+
+        // output
+        bw.write(sb.toString());
+
+        // io close
+        bw.close();
+        br.close();
+    }
+}
