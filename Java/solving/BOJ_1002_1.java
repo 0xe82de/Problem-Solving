f87a2f6a2824c4a8c1e6825dfecfe25e85a6222b
0xe82de@gmail.com <0xe82de@gmail.com>
2021-10-20 23:29:12 +0900

    init

---
+++ Java/solving/BOJ_1002_1.java
@@ -0,0 +1,33 @@
+package solving;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * BOJ 1002 터렛
+ * S4
+ * 수학, 기하학
+ */
+
+public class BOJ_1002_1 {
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+        // io
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        // logic
+
+
+        // output
+        bw.write(sb.toString());
+
+        // io close
+        bw.close();
+        br.close();
+    }
+}
