91b0bf125e5b64ff490abf8d96e4bdd97b5b5de2
0xe82de@gmail.com <0xe82de@gmail.com>
2021-11-09 17:10:42 +0900

    Create BOJ 7287 등록

---
+++ Java/boj/bronze/BOJ_7287_1.java
@@ -0,0 +1,15 @@
+package boj.bronze;
+
+/**
+ * BOJ 7287 등록
+ * B5
+ * 구현
+ */
+
+public class BOJ_7287_1 {
+
+    public static void main(String[] args) {
+        System.out.println(183);
+        System.out.println("0xe82de");
+    }
+}
