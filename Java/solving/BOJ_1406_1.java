934278405f1af8f69f919590c9d5a8da4c040167
0xe82de@gmail.com <0xe82de@gmail.com>
2021-11-10 22:08:18 +0900

    Create

---
+++ Java/solving/BOJ_1406_1.java
@@ -0,0 +1,33 @@
+package solving;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * BOJ 1406 에디터
+ * S3
+ * 자료 구조, 스택, 연결 리스트
+ */
+
+public class BOJ_1406_1 {
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+        // io
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        // logic
+
+
+        // output
+        bw.write(sb.toString());
+
+        // io close
+        bw.close();
+        br.close();
+    }
+}
