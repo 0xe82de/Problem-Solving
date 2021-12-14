21a6ae5c00223949b8926b4c5743c77a1ff941e1
김경석 <0xe82de@gmail.com>
2021-12-14 15:00:44 +0900

    Create: BOJ 15651 N과 M (3)

---
+++ Java/boj/silver/BOJ_15651_1.java
@@ -0,0 +1,56 @@
+package boj.silver;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * BOJ 15651 N과 M (3)
+ * S3
+ * 백트래킹
+ */
+
+public class BOJ_15651_1 {
+
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static boolean[] visited;
+    static int[] result;
+
+    public static void main(String[] args) throws IOException {
+        // io
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // logic
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
+        result = new int[M];
+
+        compute(0);
+
+        // output
+        bw.write(sb.toString());
+
+        // io close
+        bw.close();
+        br.close();
+    }
+
+    private static void compute(int cnt) {
+        if (cnt == M) {
+            for (int i = 0; i < M; ++i) {
+                sb.append(result[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; ++i) {
+            result[cnt] = i;
+            compute(cnt + 1);
+        }
+    }
+
+}
