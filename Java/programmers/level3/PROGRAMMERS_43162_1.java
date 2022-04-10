b9f511e7d8feafefa87cc90df5af229c60edf158
김경석 <0xe82de@gmail.com>
2022-04-10 16:59:05 +0900

    Create: PROGRAMMERS 43162 네트워크

---
+++ Java/programmers/level3/PROGRAMMERS_43162_1.java
@@ -0,0 +1,75 @@
+package programmers.level3;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+/**
+ * PROGRAMMERS 43162 네트워크
+ * Level 3
+ * DFS, BFS
+ */
+
+public class PROGRAMMERS_43162_1 {
+
+    public static void main(String[] args) {
+        // input
+        int n = 3;
+        int[][] computers = {
+                {1, 1, 0},
+                {1, 1, 0},
+                {0, 0, 1}
+        };
+
+        // logic
+        int answer = solution(n, computers);
+
+        // output
+        System.out.println("answer = " + answer);
+    }
+
+    static int[] networks;
+
+    static public int solution(int n, int[][] computers) {
+        int answer = 0;
+
+        make(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                if (computers[i][j] == 1) {
+                    union(i, j);
+                }
+            }
+        }
+
+        Set<Integer> networkTypes = new HashSet<>();
+        for (int network : networks) {
+            networkTypes.add(networks[network]);
+        }
+        answer = networkTypes.size();
+
+        return answer;
+    }
+
+    static void make(int n ) {
+        networks = IntStream.range(0, n).toArray();
+    }
+
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x < y) {
+            networks[y] = x;
+        } else {
+            networks[x] = y;
+        }
+    }
+
+    static int find(int x) {
+        if (networks[x] == x) return x;
+        return networks[x] = find(networks[x]);
+    }
+}
