d65e96452d156f1baf85aa942df7bc6e6c4dc850
김경석 <0xe82de@gmail.com>
2022-04-12 22:06:46 +0900

    Create: BOJ 11053 가장 긴 증가하는 부분 수열

---
+++ Java/boj/silver/BOJ_11053_1.java
@@ -0,0 +1,59 @@
+package boj.silver;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * BOJ 11053 가장 긴 증가하는 부분 수열
+ * S2
+ * DP
+ */
+
+public class BOJ_11053_1 {
+
+    /**
+     * 수열 A의 크기
+     * 1 <= N <= 1,000
+     */
+    static int N;
+
+    /**
+     * 1 <= A <= 1,000
+     */
+    static int[] A;
+
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        // io
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // logic
+        N = Integer.parseInt(br.readLine());
+        A = new int[N];
+        dp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int base = 0; base < N; base++) {
+            dp[base] = 1;
+            for (int compare = 0; compare < base; compare++) {
+                if (A[compare] < A[base]) {
+                    dp[base] = Math.max(dp[compare] + 1, dp[base]);
+                }
+            }
+        }
+
+        // output
+        bw.write(String.valueOf(Arrays.stream(dp).max().getAsInt()));
+
+        // io close
+        bw.close();
+        br.close();
+    }
+}
