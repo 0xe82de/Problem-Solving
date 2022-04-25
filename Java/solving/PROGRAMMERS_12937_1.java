32cddccbeee8b749c86cbdacf3781528cc236629
김경석 <0xe82de@gmail.com>
2022-04-26 03:19:14 +0900

    Create: PROGRAMMERS 12937 짝수와 홀수

---
+++ Java/solving/PROGRAMMERS_12937_1.java
@@ -0,0 +1,29 @@
+package solving;
+
+/**
+ * PROGRAMMERS 12937 짝수와 홀수
+ * Level 1
+ * 구현
+ */
+
+public class PROGRAMMERS_12937_1 {
+
+    public static void main(String[] args) {
+        // input
+        int num = 3;
+
+        // logic
+        String answer = solution(num);
+
+        // output
+        System.out.println("answer = " + answer);
+    }
+
+    static public String solution(int num) {
+        String answer = "";
+
+        answer = num % 2 != 0 ? "Odd" : "Even";
+
+        return answer;
+    }
+}
