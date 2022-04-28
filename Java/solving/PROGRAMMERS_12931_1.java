638bb40fdea6bd25460b725d8a843ef459cc9567
김경석 <0xe82de@gmail.com>
2022-04-29 02:24:52 +0900

    Create: PROGRAMMERS 12931 자릿수 더하기

---
+++ Java/solving/PROGRAMMERS_12931_1.java
@@ -0,0 +1,32 @@
+package solving;
+
+/**
+ * PROGRAMMERS 12931 자릿수 더하기
+ * Level 1
+ * 구현
+ */
+
+public class PROGRAMMERS_12931_1 {
+
+    public static void main(String[] args) {
+        // input
+        int n = 987;
+
+        // logic
+        int answer = solution(n);
+
+        // output
+        System.out.println("answer = " + answer);
+    }
+
+    static public int solution(int n) {
+        int answer = 0;
+
+        while (n > 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+
+        return answer;
+    }
+}
