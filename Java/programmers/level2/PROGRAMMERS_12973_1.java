b3488196a39da30e10a12e2f73c87f050ab01ec2
김경석 <0xe82de@gmail.com>
2022-05-18 16:24:39 +0900

    Create: PROGRAMMERS 12973 짝지어 제거하기

---
+++ Java/programmers/level2/PROGRAMMERS_12973_1.java
@@ -0,0 +1,47 @@
+package programmers.level2;
+
+import java.util.Stack;
+
+/**
+ * PROGRAMMERS 12973 짝지어 제거하기
+ * Level 2
+ * 구현
+ */
+
+public class PROGRAMMERS_12973_1 {
+
+    public static void main(String[] args) {
+        // input
+//        String s = "baabaa";
+        String s = "cdcd";
+
+        // logic
+        int answer = solution(s);
+
+        // output
+        System.out.println("answer = " + answer);
+    }
+
+    static public int solution(String s) {
+        int answer = 0;
+
+        answer = getAnswer(s);
+
+        return answer;
+    }
+
+    static int getAnswer(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+            } else if (stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
