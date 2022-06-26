b3bbf99da54e617d2282ba130c7702a43e36cdbf
김경석 <0xe82de@gmail.com>
2022-06-26 14:41:58 +0900

    Create: BOJ 1269 대칭 차집합

---
+++ Java/boj/silver/BOJ_1269_1.java
@@ -0,0 +1,51 @@
+package boj.silver;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+/**
+ * BOJ 1269 대칭 차집합
+ * ??
+ * 자료 구조, 해시를 사용한 집합과 맵, 트리를 사용한 집합과 맵
+ */
+
+public class BOJ_1269_1 {
+
+    public static void main(String[] args) throws IOException {
+        // io
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // logic
+        Set<String> aSet = new HashSet<>();
+        Set<String> aSet2 = new HashSet<>();
+        Set<String> bSet = new HashSet<>();
+
+        br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        while (st.hasMoreTokens()) {
+            String number = st.nextToken();
+            aSet.add(number);
+            aSet2.add(number);
+        }
+
+        st = new StringTokenizer(br.readLine(), " ");
+        while (st.hasMoreTokens()) {
+            bSet.add(st.nextToken());
+        }
+
+        aSet.removeAll(bSet);
+        bSet.removeAll(aSet2);
+
+        int count = aSet.size() + bSet.size();
+
+        // output
+        bw.write("" + count);
+
+        // io close
+        bw.close();
+        br.close();
+    }
+}
